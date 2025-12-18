e1388fc9f3769dcf4b18009301b0b7973d8f915e
Swochhanda14 <swochhanda14@gmail.com>
2025-12-18 15:37:30 +0545

    edit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Portfoli_website_react/",
   plugins: [tailwindcss(), react()],
 });
